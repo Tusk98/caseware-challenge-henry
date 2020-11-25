f079f0f133f429b81485c65d2af524fc30288c6a
Tusk98 <henryracoomb@gmail.com>
2020-11-25 18:36:43 -0500

    Trying to get sever working

--- app/app.js
+++ app/app.js
@@ -1,36 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
-const port = process.env.PORT || 4041;
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const users = [{name: 'Tony', email: 'tony@mail.com'}]
-
-app.get('/', (_, res) => {
-  res.send('Express App');
+app.post('/example', (req, res) => {
+  res.send(`This some text`);
 });
 
-app.get('/users', (_, res) => {
-  res.json({ok: true, users});
-});
-
-app.get('/user/:name', (req, res) => {
-  const { name } = req.params;
-  const user = users.filter((user) => user.name == name)[0];
-  res.json({ok: true, user});
-});
-
-app.post('/adduser', (req,res) => {
-  const { name, email} = req.body;
-  if(name && email) {
-    users.push({name, email});
-    res.json({ok: true, users});
-  }
-});
+const port = 8080;
 
 app.listen(port, () => {
-  console.log(`server is running on: ${port}`);
+  console.log(`Server running on port ${port}`);
 });
