35a58a1c5124ef812abd7d72e7a8617fe5fa83c9
Tusk98 <henryracoomb@gmail.com>
2020-11-26 14:48:41 -0500

    Got nodejs backend working

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const http = require("http");
 const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
 const util = require("util");
-var Luhn = require('luhn-js');
+
+var port = process.env.PORT || 1234;
 
 const server = http.createServer(function(req, res) {
   //console.log(req.headers)
   let path = url.parse(req.url, true);
-  var valid_cc = Luhn.isValid(path.query['ccnum']);
+  var valid_cc = luhn_algo(parseInt(path.query['ccnum']));
 
   if ( valid_cc == 1) {
     res.writeHead(200, "OK");
@@ -16,11 +17,32 @@ const server = http.createServer(function(req, res) {
   } else {
     res.writeHead(400, "not ok");
     res.write(util.inspect(path.query) + "\n\n");
-
     res.end('End of msg to browser');
   }
 });
 
-server.listen(1234, function() {
+server.listen(port, function() {
   console.log("Listening on port 1234");
 });
+
+// Takes a credit card string value and returns true on valid number
+function luhn_algo(value) {
+  // Accept only digits, dashes or spaces
+	if (/[^0-9-\s]+/.test(value)) return false;
+
+	// The Luhn Algorithm. It's so pretty.
+	let nCheck = 0, bEven = false;
+	value = value.toString().replace(/\D/g, "");
+
+	for (var n = value.length - 1; n >= 0; n--) {
+		var cDigit = value.charAt(n),
+			  nDigit = parseInt(cDigit, 10);
+
+		if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
+
+		nCheck += nDigit;
+		bEven = !bEven;
+	}
+
+	return (nCheck % 10) == 0;
+}
