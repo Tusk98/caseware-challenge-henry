c3b9c56120d7b9965705cff2bf23ef4550bf38f2
Tusk98 <henryracoomb@gmail.com>
2020-11-25 22:59:28 -0500

    Got a nodejs working

--- app/app.js
+++ app/app.js
@@ -1,15 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const http = require("http");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
+const util = require("util");
+var Luhn = require('luhn-js');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+const server = http.createServer(function(req, res) {
+  //console.log(req.headers)
+  let path = url.parse(req.url, true);
+  var valid_cc = Luhn.isValid(path.query['ccnum']);
 
-app.post('/example', (req, res) => {
-  res.send(`This some text`);
+  if ( valid_cc == 1) {
+    res.writeHead(200, "OK");
+    res.end('Success');
+
+  } else {
+    res.writeHead(400, "not ok");
+    res.write(util.inspect(path.query) + "\n\n");
+
+    res.end('End of msg to browser');
+  }
 });
 
-const port = 8080;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+server.listen(1234, function() {
+  console.log("Listening on port 1234");
 });
